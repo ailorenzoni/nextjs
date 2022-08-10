bb5b0eafd824d48072b8cdde037ab20f59f39493
Ailorenzoni <ailorenzoni-news@yahoo.com.br>
2022-08-10 16:14:31 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Odontoworks</a>
+          Welcome to <a href="https://nextjs.org">Odontoworks.org</a>
         </h1>
       </main>
 
